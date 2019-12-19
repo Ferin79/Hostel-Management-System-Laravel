82ac94ba72d5b44b3c0ee2752206847a813125e8
Ferin79 <ferinpatel79@gmail.com>
2019-12-19 21:00:12 +0530

    Landing Page Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,23 +7,17 @@
         <title>Home</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
                 margin: 0;
+                overflow: hidden;
             }
-
-            .full-height {
-                height: 100vh;
-            }
-
             .flex-center {
                 align-items: center;
                 display: flex;
@@ -36,18 +30,9 @@
 
             .top-right {
                 position: absolute;
-                right: 10px;
+                right: 25px;
                 top: 18px;
             }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -57,14 +42,87 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .landing-wrapper
+            {
+                padding-top: 50px;
+                width: 80%;
+                height: 90vh;
+            }
+            .landing-wrapper .row
+            {
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+            }
+            .landing-img
+            {
+                padding-top: 100px;
+                height: 500px;
+                width: 750px;
+            }
+            .landing-part-1, .landing-part-2
+            {
+                width: 50%;
+            }
+            .landing-part-1 h1
+            {
+                color: #0984e3;
+                font-size: 50px;
+                font-weight: bold;
+            }
+            .landing-btn-1, .landing-btn-2
+            {
+                text-transform: uppercase;
+                margin: 20px;
+                padding: 20px;
+                background: none;
+                border: none;
+                font-size: 20px;
+                border-radius: 20px;
+                color: white;
+            }
+            .landing-btn-1
+            {
+                background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);
+            }
+            .landing-btn-2
+            {
+                background-image: linear-gradient(120deg, #f093fb 0%, #f5576c 100%);
+            }
+            @media(max-width: 900px) {
+                .landing-wrapper .row
+                {
+                    display: block;
+                }
+                .landing-part-1, .landing-part-2
+                {
+                    width: 100%;
+                }
+                .landing-part-1 h1
+                {
+                    color: #0984e3;
+                    font-size: 35px;
+                    font-weight: bold;
+                }
+                .landing-btn-1, .landing-btn-2
+                {
+                    padding: 15px;
+                    margin: 10px;
+                    margin-left: 0px;
+                    font-size: 15px;
+                }
+                .landing-img
+                {
+                    padding-top: 50px;
+                    height: 300px;
+                    width: 300px;
+                }
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="flex-center position-ref">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,22 +137,21 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="container landing-wrapper">
+                <div class="row">
+                    <div class="col-6 landing-part-1">
+                        <h1>Home Away</h1>
+                        <h1>From Home</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cum dolorum, earum est fuga id minus quae saepe temporibus voluptatibus.</p>
+                        <button class="landing-btn-1">Watch Video</button>
+                        <button class="landing-btn-2">Learn More</button>
+                    </div>
+                    <div class="col-6 landing-part-2">
+                        <img class="img-fluid landing-img" src="{{url('images/landing.svg ')}}">
+                    </div>
                 </div>
             </div>
+
         </div>
     </body>
 </html>
